3b8a31d8c829abecb2501a63f28545aa44d8a57e
Solange <sole.agp@gmail.com>
2024-07-11 21:39:23 -0400

    Se agrega función de smooth scroll mediante archivo 'script.js'

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("a").on('click', function (event) {   //click en opciones del menú
+        if (this.hash !== "") {             //asegura precencia de link
+            event.preventDefault();         //previene cambio abrupto
+            var hash = this.hash;
+
+            $('html, body').animate({           
+                scrollTop: $(hash).offset().top  //smooth scroll 
+            }, 800, function () {
+                
+                window.location.hash = hash;     //cambia url
+            });
+        }
+    });
+});
